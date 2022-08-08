f2f2add411381b0c0830d0ed79167ffd43bf9e97
Alexander <alexandrkudryash@mail.ru>
2022-08-08 19:24:44 +0300

    added folder Home Work Seminar 1 with Task 1 to the traced repository

---
+++ HomeworkSeminar1-040822/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача-1  По двум заданным числам проверять является ли первое число квадратом второго
+
+int numberA = 16;
+int numberB = 5;
+int result = numberA/numberB;
+
+if(result == numberB)
+   {
+    Console.WriteLine("Число B является квадратом числа А");
+   }
+else
+   {
+    Console.WriteLine("Число B не является квадратом числа А");
+   }

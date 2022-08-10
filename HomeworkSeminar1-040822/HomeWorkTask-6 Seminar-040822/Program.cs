1b62f5fda9f8669c9e69f352879cf648f9884992
Alexander <alexandrkudryash@mail.ru>
2022-08-10 22:00:58 +0300

    added Task-6 Please show the 3rd number of tree figures

---
+++ HomeworkSeminar1-040822/HomeWorkTask-6 Seminar-040822/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача-6: Показать последнюю цифру трёхзначного числа
+// ДЗ к Семинару 1 от 04.08.2022 
+
+Console.WriteLine("Введите любое трёхзначное число");
+int a = int.Parse(Console.ReadLine());
+int b = a/100;
+int c = a - (b*100);
+int d = c/10;
+int N = c -(d*10);
+Console.Write("Третья цифра введённого числа = ");
+Console.WriteLine(N);

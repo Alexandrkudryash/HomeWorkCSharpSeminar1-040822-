cdc55577b9964f8026ff585fa1a7f5278f976d4a
Alexander <alexandrkudryash@mail.ru>
2022-08-10 20:55:01 +0300

    added Task 4 Checking whether the number is even or odd number

---
+++ HomeworkSeminar1-040822/HomeWorkTask-4 Seminar-040822/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задача-4: Выяснить является ли число четным.
+// ДЗ к Семинару 1 от 04.08.2022 
+
+Console.WriteLine("введите число, проверим оно чётное или нет");
+int a = int.Parse(Console.ReadLine());
+
+if ( a%2 == 0) 
+{
+    Console.Write("введенное число ");
+    Console.Write(a);
+    Console.WriteLine(" является чётным");
+}
+else
+{
+    Console.Write("введенное число ");
+    Console.Write(a);
+    Console.WriteLine(" чётным НЕ является!");
+}
+

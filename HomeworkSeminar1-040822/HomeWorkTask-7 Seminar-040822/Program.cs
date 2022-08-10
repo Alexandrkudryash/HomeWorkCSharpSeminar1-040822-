d52f243fcde1135a8305701bc751ba52c52820d1
Alexander <alexandrkudryash@mail.ru>
2022-08-10 22:08:28 +0300

    added Task 7 - please show the second number of tree figures

---
+++ HomeworkSeminar1-040822/HomeWorkTask-7 Seminar-040822/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача-7: Показать вторую цифру трёхзначного числа
+// ДЗ к Семинару 1 от 04.08.2022
+
+Console.WriteLine("Введите любое трёхзначное число");
+int a = int.Parse(Console.ReadLine());
+int b = a/100;
+int c = a - (b*100);
+int d = c/10;
+
+Console.Write("Вторая цифра введённого числа = ");
+Console.WriteLine(d);

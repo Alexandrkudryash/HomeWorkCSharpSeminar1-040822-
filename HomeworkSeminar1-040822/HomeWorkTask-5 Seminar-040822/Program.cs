83f1b964bed50ee7e2c7d8d7fe59dbcb11a53f8c
Alexander <alexandrkudryash@mail.ru>
2022-08-10 21:17:12 +0300

    added Task 6 Please show numbers from -N to N

---
+++ HomeworkSeminar1-040822/HomeWorkTask-5 Seminar-040822/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача-5: Показать числа от  - N до N
+// ДЗ к Семинару 1 от 04.08.2022 
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+int count = N*(-1);
+while (count <= N )
+{
+    Console.WriteLine(count);
+    count++;
+}

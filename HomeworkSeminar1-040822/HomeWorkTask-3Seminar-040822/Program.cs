378e166ac64a5378ce6d9c5f68c55b28d5fd0513
Alexander <alexandrkudryash@mail.ru>
2022-08-10 20:35:16 +0300

    added Task 3 Find Max of 3 numbers

---
+++ HomeworkSeminar1-040822/HomeWorkTask-3Seminar-040822/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Задача-3: Найти максимальное из трех чисел
+// ДЗ к Семинару 1 от 04.08.2022 
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write ("Максимальное из введённых чисел = ");
+Console.WriteLine(max);
+

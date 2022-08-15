f8127714013d7e86fb41023df9326707d9301729
Alexander <alexandrkudryash@mail.ru>
2022-08-15 19:30:09 +0300

    added to git Task 1,2,8,11

---
+++ HomeworkSeminar1-040822/HomeWorkTask-1 Seminar-040822/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача-1  По двум заданным числам проверять является ли первое число квадратом второго
+// ДЗ к Семинару 1 от 04.08.2022 
+
+int numberA = 16;
+int numberB = 4;
+int result = numberA/numberB;
+
+if(result == numberB)
+   {
+    Console.WriteLine("Число B является квадратом числа А");
+   }
+else
+   {
+    Console.WriteLine("Число B не является квадратом числа А");
+   }

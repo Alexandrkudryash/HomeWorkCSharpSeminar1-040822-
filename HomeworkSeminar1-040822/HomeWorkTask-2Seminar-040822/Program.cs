21ffd3e0cc2d1772ebceb429b04045f6ccdff4af
Alexander <alexandrkudryash@mail.ru>
2022-08-15 19:24:01 +0300

    Modified Task2 and Task 1 added as separate folder

--- HomeworkSeminar1-040822/HomeWorkTask-2Seminar-040822/Program.cs
+++ HomeworkSeminar1-040822/HomeWorkTask-2Seminar-040822/Program.cs
@@ -1,9 +1,12 @@
 ﻿// Задача-2: По заданному номеру дня недели вывести его название
 // ДЗ к Семинару 1 от 04.08.2022 
 
+Console.Clear();
 Console.WriteLine("Введите номер дня недели");
-int d = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine() ?? "0");
 
+if (d > 0 && d < 8)
+{
 if (d == 1) { Console.WriteLine("Сегодня Понедельник"); }
 if (d == 2) { Console.WriteLine("Сегодня Вторник");}
 if (d == 3) { Console.WriteLine("Сегодня Среда");}
@@ -11,6 +14,7 @@ if (d == 4) { Console.WriteLine("Сегодня Четверг");}
 if (d == 5) { Console.WriteLine("Сегодня Пятница");}
 if (d == 6) { Console.WriteLine("Сегодня Суббота");}
 if (d == 7) { Console.WriteLine("Сегодня Воскресенье");}
+}
 else
 {
     Console.WriteLine("Набранное число не является днём недели");
